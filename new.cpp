dd77a072bd4faf4fac5c891599386d1aae8b18c1
R_Hasan <sohandoabernahin1@gmail.com>
2020-11-16 15:37:32 +0600

    updated

--- new.cpp
+++ new.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int dp[1<<21];
-int fib(long long int a, int b) {
-  if (dp[a % b] != -1) return dp[a % b];
-  if (a % b == 0) return 0 % b;
-  else if (a % b == 1) return 1 % b;
-  return dp[a % b] = ((fib(a - 2, b) % b + fib(a - 1, b) % b) % b);
+int fib(int a, int b) {
+  if (dp[a] != -1) return dp[a];
+  if (a == 0) return 0 % b;
+  else if (a == 1) return 1 % b;
+  return dp[a] = ((fib(a - 2, b) % b + fib(a - 1, b) % b) % b);
 }
-inline int mod(long long int a, int b) {
+inline int mod(int a, int b) {
   return fib(a, b);
 }
-void solve(long long int n, int m) {
+void solve(int n, int m) {
   memset(dp, -1, sizeof dp);
   function<int(int, int)> _pow = [&](int a, int b) {    
     int ret = 1;
@@ -29,8 +29,7 @@ void solve(long long int n, int m) {
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int m;
-  long long int n;
+  int n, m;
   while (cin >> n >> m) {
     solve(n, m);
   }
